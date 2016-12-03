8c30ae6f41df87f70fb191e0510625ff00c57b2a
Tangrila-BG <phatomiki@gmail.com>
2016-12-03 17:05:46 +0200

    better form styling

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -3,31 +3,44 @@ import React, {Component} from 'react'
 export default class LoginForm extends Component {
     render() {
         return (
-            <main>
-                <form onSubmit={this.props.onSubmitHandler}>
-                <div className="form-group">
-                    <label>Username:</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="username"
-                        value={this.props.username}
-                        disabled={this.props.submitDisabled}
-                        onChange={this.props.onChangeHandler}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Password:</label>
-                    <input
-                        className="form-control"
-                        type="password"
-                        name="password"
-                        value={this.props.password}
-                        disabled={this.props.submitDisabled}
-                        onChange={this.props.onChangeHandler}
-                    />
-                </div>
-                <input className="btn btn-default" type="submit" value="Login" disabled={this.props.submitDisabled}/>
+            <main className="row">
+                <form className="form-horizontal" onSubmit={this.props.onSubmitHandler}>
+                    <div className="form-group">
+                        <label className="col-sm-2 control-label">Username:</label>
+	                    <div className="col-sm-3">
+		                    <input
+			                    className="form-control"
+			                    type="text"
+                                name="username"
+                                value={this.props.username}
+                                disabled={this.props.submitDisabled}
+                                onChange={this.props.onChangeHandler}
+                                required
+		                    />
+	                    </div>
+                    </div>
+                    <div className="form-group">
+                        <label className="col-sm-2 control-label">Password:</label>
+	                    <div className="col-sm-3">
+                            <input
+	                            className="form-control"
+                                type="password"
+                                name="password"
+                                value={this.props.password}
+                                disabled={this.props.submitDisabled}
+                                onChange={this.props.onChangeHandler}
+                                required
+                            />
+	                    </div>
+                    </div>
+	                <div className="form-group">
+	                    <div className="col-sm-3 col-sm-offset-3">
+		                    <input className="btn btn-default"
+		                           type="submit" value="Login"
+		                           disabled={this.props.submitDisabled}
+		                    />
+	                    </div>
+	                </div>
             </form>
         </main>
         )
