63fe3d6b6e60d9b1fe733c5b8bf0ced5c792e895
Tangrila-BG <phatomiki@gmail.com>
2016-12-03 13:23:18 +0200

    renaming

--- src/components/Register/RegisterPage.js
+++ src/components/Register/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import RegisterForm from './RegisterForm'
-import User from '../../models/user'
+import User from '../../models/userModel'
 let user = new User();
 
 export default class RegisterPage extends Component {
