6c9a68b210335b0de6469e098699d06ff534c65e
G A <fifty_cen7@abv.bg>
2016-12-06 20:02:12 +0200

    removed lines from pictures
    removed lines from pictures

--- src/components/common/Test.js
+++ src/components/common/Test.js
@@ -51,22 +51,19 @@ export default class Test extends Component {
 				<Carousel.Item>
 					<img width={700} height={450} alt="900x500" src={this.pictures[0]}/>
 					<Carousel.Caption>
-						<h3>First slide label</h3>
-						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+						<h3>Welcome to SoftUni Forum</h3>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
 					<img width={700} height={450} alt="900x500" src={this.pictures[1]}/>
 					<Carousel.Caption>
-						<h3>Second slide label</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<h3>Welcome to SoftUni Forum</h3>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
 					<img width={700} height={450} alt="900x500" src={this.pictures[2]}/>
 					<Carousel.Caption>
-						<h3>Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+						<h3>Welcome to SoftUni Forum</h3>
 					</Carousel.Caption>
 				</Carousel.Item>
 			</Carousel>
