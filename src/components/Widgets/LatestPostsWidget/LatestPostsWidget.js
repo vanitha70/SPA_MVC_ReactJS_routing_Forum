5a092cd71b7015ac73ca9dda9219ecdf09fb399a
Tangrila-BG <phatomiki@gmail.com>
2016-12-07 01:48:02 +0200

    Latest posts now appear on the home page

---
+++ src/components/Widgets/LatestPostsWidget/LatestPostsWidget.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react'
+import Post from '../../../models/postModel';
+import View from '../../../models/viewsModel';
+import {Link} from 'react-router';
+let postModule = new Post();
+let viewModule = new View();
+
+/*
+ Gets all posts and views. Sorts the posts by
+ posted time and displays 12 posts on 3 columns
+ */
+
+export default class LatestPostsWidget extends Component {
+	constructor(props) {
+		super(props);
+		this.bindFunctionsToParent();
+		this.state = {
+			latestPosts: []
+		};
+console.log()
+	}
+
+	bindFunctionsToParent() {
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.onFetchDataSuccess = this.onFetchDataSuccess.bind(this);
+	}
+
+	componentDidMount() {
+		let requests = [
+			postModule.query('{}', `sort={"_kmd":{"etc": ${1}}&limit=${12}`),
+			viewModule.getAllViews()
+		];
+		Promise.all(requests).then(this.onFetchDataSuccess);
+	}
+
+	onFetchDataSuccess([posts, views]) {
+		for (let post of posts)
+			post.rating = views.filter(view => view.postId === post._id)[0].rating;
+		this.setState({latestPosts: posts});
+	}
+
+	render() {
+		// prep posts
+		let postsForRender = Array.from(this.state.latestPosts)
+			.map(function (post) {
+				let rating = Math.floor(post.rating / 10);
+				let ratingRender = [];
+				let timestamp = '';
+
+				if (rating > 5)
+					rating = 5;
+
+				// takes the time
+				for (let prop in post._kmd)
+					if (prop.localeCompare('ect'))
+						timestamp = post._kmd[prop].substr(0, 16).replace('T', ' ');
+
+				let i = 0;
+				for (; i < rating; i++)
+					ratingRender.push(<span key={i} className="glyphicon glyphicon-star"></span>)
+				while (ratingRender.length < 5)
+					ratingRender.push(<span key={i++} className="glyphicon glyphicon-star-empty"></span>);
+				
+				return (
+					<div key={post._id} className="col-sm-4 col-lg-4 col-md-4">
+
+						<Link style={{ textDecoration: "none" }} to={`/posts/details/${post._id}`}>
+							<div className="thumbnail btn-custom">
+									<div className="caption post-body">
+										<div className="text-center">
+											<h3>
+												{post.title}
+											</h3>
+										</div>
+										<div>{post.body.substr(0, 1000)}</div>
+									</div>
+								<div className="post-views">
+									<p className="pull-right views">{post.rating} views</p>
+									<p>{timestamp}</p>
+									<div className="post-rating text-center">
+										{ratingRender}
+									</div>
+								</div>
+							</div>
+						</Link>
+
+					</div>
+				)
+			});
+
+		return (
+			<div className="col-md-12">
+				<div className="row">
+					<div className="page-header text-center">
+						<h1>Latest Posts</h1>
+					</div>
+					{postsForRender}
+				</div>
+			</div>
+
+		)
+	}
+}
