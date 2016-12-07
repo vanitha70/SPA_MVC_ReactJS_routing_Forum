554e9bf47ab6bb6c1727440587552dc7d6f0fb08
Kamen <kamenminkov.com@abv.bg>
2016-12-07 11:06:25 +0200

    Show less

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -5,7 +5,6 @@ export default class LoginForm extends Component {
         return (
         	<div className="card card-container">
 		        <img id="profile-img" alt="Profile" className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-		        <p id="profile-name" className="profile-name-card">{sessionStorage.getItem('username')}</p>
 		        <form className="form-signin" onSubmit={this.props.onSubmitHandler}>
 			        <div className="form-group">
 				        <input
